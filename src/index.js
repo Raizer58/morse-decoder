0fe4d38f0b235a5bad4667410b0298f539a3562b
Raizer58 <Raizer9058w@gmail.com>
2020-03-17 21:30:26 +0300

    feat: added morse-decoder solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let morseDecode = new String();
+
+  for(let i = 0; i < expr.length; i+=10){
+    let morseSymbol = expr.slice(i,i+10)
+    let morseDecodeSymbol = new String();
+
+    for(let j = 0; j < morseSymbol.length; j+=2){
+      if (morseSymbol.slice(j,j+2) == "10"){
+        morseDecodeSymbol += ".";
+      }
+      else if (morseSymbol.slice(j,j+2) == "11"){
+        morseDecodeSymbol += "-";
+      }
+    };
+
+    if (morseSymbol == "**********"){
+      morseDecode += " ";
+      continue;
+    };
+
+    morseDecode += MORSE_TABLE[morseDecodeSymbol]
+  };
+
+  return morseDecode;
+};
 
 module.exports = {
     decode
